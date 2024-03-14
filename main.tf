0aa3810c4d53cc35ad5ab0ce8ec9aa4dcf983065
DumalBhaskar <103929819+DumalBhaskar@users.noreply.github.com>
2024-03-14 18:21:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,13 +10,13 @@ terraform {
 }
 
 provider "aws" {
-  access_key = ""
-  secret_key = ""
-  region = "us-west-2"
+  access_key = "AKIAYU7H7SN4QIHBNCNH"
+  secret_key = "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"
+  region = "ap-south-1"
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-03bb6d83c60fc5f7c"
   instance_type = "t2.micro"
 
   tags = {
