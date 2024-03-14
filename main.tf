73b8606af31d8eaa267a5ef886a5909158c6a7d2
DumalBhaskar <103929819+DumalBhaskar@users.noreply.github.com>
2024-03-14 14:00:49 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  access_key = "AKIAYU7H7SN4VQ6VKLJG"
+  secret_key = "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"
+  region = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
